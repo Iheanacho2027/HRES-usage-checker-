cfd1bdcfc1ce28f7af5d8c4e5ec22d193de03049
Iheanacho2027 <iheanachodenwigwe@gmail.com>
2023-03-16 19:06:22 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Prints "showing a proper documented and incident showing of a glamorous
- *               showing and content of art" started by a new line.
+ * main - Prints "showing a proper documented and incident showing of a glamorous" started by a new line.
  *
  * Return: Always 0.
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
 	return (0);
 }
