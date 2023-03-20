3c28cf69d155c6dcb2636ff1cdeb775d12d3e8f9
Iheanacho2027 <iheanachodenwigwe@gmail.com>
2023-03-20 12:23:04 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,7 +14,7 @@ int main(void)
            if (n > 0) {
                printf("is positive");
                         } 
-	   else if (n = 0) {
+	   else if (n == 0) {
                printf("is zero");
                         }
 	   else (n < 0) {
